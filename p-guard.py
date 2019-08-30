2e3fbee48e84efee9bd17b8b7ef4a7c9bc7d3bdc
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-08-30 20:16:06 +0700

    Update p-guard.py

--- p-guard.py
+++ p-guard.py
@@ -1,8 +1,8 @@
 # uncompyle6 version 3.3.3
 # Python bytecode 2.7
 # Decompiled from: Python 2.7.16 (default, Apr 24 2019, 10:05:31) 
-# [GCC 4.2.1 Compatible Android (5058415 based on r339409) Clang 8.0.2 (https://a
-# Embedded file name: <debby>
+# [GCC 4.2.1 Compatible Android (5058415 based on r339409) Clang 8.0.2
+# Embedded file name: <guard>
 import requests, json, os, sys, hashlib
 n = []
 
@@ -36,11 +36,13 @@ def turn_shield(token, enable=True):
             print '\x1b[91mProfil Guard Is Non-Active '
         else:
             print 'Error'
-print '\x1b[94m  ____                     _'
-print '\x1b[94m / ___|_   _  __ _ _ __ __| |'
-print '\x1b[94m| |  _| | | |/ _, | ,__/ _. |'
-print '\x1b[94m| |_| | |_| | (_| | | | (_| |'
-print '\x1b[94m \____|\__,_|\__,_|_|  \__,_|' print '\x1b[92mV.1.1'
+print '\x1b[94m ▄▀▀▀▀▄   ▄▀▀▄ ▄▀▀▄  ▄▀▀█▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄  '
+print '\x1b[94m█        █   █    █ ▐ ▄▀ ▀▄ █   █   █ █ ▄▀   █ '
+print '\x1b[94m█    ▀▄▄ ▐  █    █    █▄▄▄█ ▐  █▀▀█▀  ▐ █    █ '
+print '\x1b[94m█     █ █  █    █    ▄▀   █  ▄▀    █    █    █ '
+print '\x1b[94m▐▀▄▄▄▄▀ ▐   ▀▄▄▄▄▀  █   ▄▀  █     █    ▄▀▄▄▄▄▀ '
+print '\x1b[94m▐                   ▐   ▐   ▐     ▐   █     ▐  '
+print '\x1b[94m                                      ▐        ' print '\x1b[90mV.r.1.0'
 
 print '\x1b[94m[=============================================]'
 print '\x1b[95m[ Tool     : Active/Non-Active Guard Facebook ]'
@@ -66,11 +68,13 @@ except KeyError:
     print '\x1b[91m[!] Login Failed'
     print '\x1b[91m[!] Check Email or Password Again'
     exit()
-print '\x1b[94m  ____                     _'
-print '\x1b[94m / ___|_   _  __ _ _ __ __| |'
-print '\x1b[94m| |  _| | | |/ _. | .__/ _. |'
-print '\x1b[94m| |_| | |_| | (_| | | | (_| |'
-print '\x1b[94m \____|\__,_|\__,_|_|  \__,_|' print '\x1b[92mV.1.1'
+print '\x1b[94m ▄▀▀▀▀▄   ▄▀▀▄ ▄▀▀▄  ▄▀▀█▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄  '
+print '\x1b[94m█        █   █    █ ▐ ▄▀ ▀▄ █   █   █ █ ▄▀   █ '
+print '\x1b[94m█    ▀▄▄ ▐  █    █    █▄▄▄█ ▐  █▀▀█▀  ▐ █    █ '
+print '\x1b[94m█     █ █  █    █    ▄▀   █  ▄▀    █    █    █ '
+print '\x1b[94m▐▀▄▄▄▄▀ ▐   ▀▄▄▄▄▀  █   ▄▀  █     █    ▄▀▄▄▄▄▀ '
+print '\x1b[94m▐                   ▐   ▐   ▐     ▐   █     ▐  '
+print '\x1b[94m                                      ▐        ' print '\x1b[90mV.r.1.0'
 print '\x1b[0mPilih jenis fungsi guard'
 print '\x1b[93m[01] Activate'
 print '\x1b[93m[02] Non-Active'
