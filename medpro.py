e18687c44893c40e85cce91e73cb04a8e07a41d6
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-01 19:50:10 +0700

    Update and rename p-guard.py to medpro.py

--- medpro.py
+++ medpro.py
@@ -35,14 +35,33 @@ def turn_shield(token, enable=True):
         if '"is_shielded":false' in res.text:
             print '\x1b[91mProfil Guard Is Non-Active '
         else:
-            print 'Error'
-print '\x1b[94m ▄▀▀▀▀▄   ▄▀▀▄ ▄▀▀▄  ▄▀▀█▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄  '
-print '\x1b[94m█        █   █    █ ▐ ▄▀ ▀▄ █   █   █ █ ▄▀   █ '
-print '\x1b[94m█    ▀▄▄ ▐  █    █    █▄▄▄█ ▐  █▀▀█▀  ▐ █    █ '
-print '\x1b[94m█     █ █  █    █    ▄▀   █  ▄▀    █    █    █ '
-print '\x1b[94m▐▀▄▄▄▄▀ ▐   ▀▄▄▄▄▀  █   ▄▀  █     █    ▄▀▄▄▄▄▀ '
-print '\x1b[94m▐                   ▐   ▐   ▐     ▐   █     ▐  '
-print '\x1b[94m                                      ▐        ' print '\x1b[90mV.r.1.0'
+            
+def print_cow():
+    print(" ___________ ")
+    print(" \033[07m  medpro.py! \033[27m                # Common")
+    print("      \                     # User")
+    print("       \   \033[1;31m,__,\033[1;m             # Passwords")
+    print("        \  \033[1;31m(\033[1;moo\033[1;31m)____\033[1;m         # Profiler")
+    print("           \033[1;31m(__)    )\ \033[1;m  ")
+    print(
+        "           \033[1;31m   ||--|| \033[1;m\033[05m*\033[25m\033[1;m      [ Muris Kurgas | j0rgan@remote-exploit.org ]"
+    )
+    print(28 * " " + "[ Mebus | https://github.com/Mebus/]\r\n")
+
+
+def version():
+    """Display version"""
+
+    print("\r\n	\033[1;31m[ medpro.py! ]  " + __version__ + "\033[1;m\r\n")
+    print("	* Hacked up by j0rgan - j0rgan@remote-exploit.org")
+    print("	* http://www.remote-exploit.org\r\n")
+    print("	Take a look ./README.md file for more info about the program\r\n")
+
+
+def improve_dictionary(file_to_open):
+    """Implementation of the -w option. Improve a dictionary by
+    interactively questioning the user."""
+
 
 print '\x1b[94m[=============================================]'
 print '\x1b[95m[ Tool     : Active/Non-Active Guard Facebook ]'
@@ -68,13 +87,7 @@ except KeyError:
     print '\x1b[91m[!] Login Failed'
     print '\x1b[91m[!] Check Email or Password Again'
     exit()
-print '\x1b[94m ▄▀▀▀▀▄   ▄▀▀▄ ▄▀▀▄  ▄▀▀█▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄  '
-print '\x1b[94m█        █   █    █ ▐ ▄▀ ▀▄ █   █   █ █ ▄▀   █ '
-print '\x1b[94m█    ▀▄▄ ▐  █    █    █▄▄▄█ ▐  █▀▀█▀  ▐ █    █ '
-print '\x1b[94m█     █ █  █    █    ▄▀   █  ▄▀    █    █    █ '
-print '\x1b[94m▐▀▄▄▄▄▀ ▐   ▀▄▄▄▄▀  █   ▄▀  █     █    ▄▀▄▄▄▄▀ '
-print '\x1b[94m▐                   ▐   ▐   ▐     ▐   █     ▐  '
-print '\x1b[94m                                      ▐        ' print '\x1b[90mV.r.1.0'
+print '\x1b[94m..                                        ' print '\x1b[90mV.r.1.0'
 print '\x1b[0mPilih jenis fungsi guard'
 print '\x1b[93m[01] Activate'
 print '\x1b[93m[02] Non-Active'
