77601e6e2cb07475b4d6014474665fb6f0ae900b
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-09 12:21:30 +0700

    Update DarkF8.py

--- DarkF8.py
+++ DarkF8.py
@@ -19,7 +19,7 @@ sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('User-Agent', Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36')]
+br.addheaders = [('User-Agent', Mozilla/5.0 (Linux; U; Android 8.0.0; SM-A600G Build/R16NW; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36 OPR/43.2.2254.140293')]
 
 def keluar():
     print '\x1b[1;91m[!] Tutup'
