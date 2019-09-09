034720ee6b00e03df09e7293c66bbc7f786ae40c
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-09 15:29:45 +0700

    Update DarkF8.py

--- DarkF8.py
+++ DarkF8.py
@@ -40,7 +40,7 @@ def tik():
     for o in titik:
         print '\r\x1b[1;91m[\xe2\x97\x8f] \x1b[1;92mLoading \x1b[1;97m' + o,
         sys.stdout.flush()
-        time.sleep(0.01)
+        time.sleep(30)
 
 
 back = 0
